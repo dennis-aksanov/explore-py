b7e047237188d38b88cc98975b7c62ed1cfed412
Dennis Aksanov <denis.aksanov.13@gmail.com>
2024-07-17 22:10:13 +0700

    Updated dialogue

--- src/program01.py
+++ src/program01.py
@@ -7,21 +7,32 @@ fruits = {
     "watermelon": 1,
     "pear":       7}
 
+
 name = input('Type your name: \n  ')
 print("Hello, {}".format(name))
 print("how are you {}".format(name))
 det = input(' ')
-fruit = input('choose fruit: \n  ')
-print("you choose {}".format(fruit))
-if None != fruits.get(fruit):
-    print("i have {} {}".format(fruits[fruit], fruit))
-else:
-    print("i don't have {} fruit".format(fruit))
 
-fet = input('how many fruits do you want?\n  ')
+isFruitChoosen = False
+while isFruitChoosen == False:
+    fruit = input('choose fruit: \n  ')
+    print("you choose {}".format(fruit))
+    if None != fruits.get(fruit):
+        print("i have {} {}".format(fruits[fruit], fruit))
+        isFruitChoosen = True
+    else:
+        print("i don't have {} ".format(fruit))
+        fer = input("do you want choose another one? (yes/no)\n  ")
+        if fer == "yes":
+           isFruitChoosen = False
+        else:
+            exit()
+fet = input('how many {} do you want?\n  '.format(fruit))
 fet = int(fet)
-if fruits.get(fruit) >= fet:
-    print('yes i can give you {} fruits'.format(fet))
+if fruits.get(fruit) >= fet and 0 <= fet:
+    print('yes i can give you {} {}'.format(fet, fruit))
+    fruits[fruit] = fruits[fruit] - fet
+    print("now i have {}".format(fruits[fruit]))
 else:
-    print("sorry i don't have enough fruits")
+    print("sorry i don't have enough {}".format(fruit))
 
